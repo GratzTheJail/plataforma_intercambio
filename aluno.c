052804901d8474a9c31cc92717d8b4a72af0f0a3
GratzTheJail <guigagratz@hotmail.com>
2025-06-15 16:33:16 -0300

    add nomes e matriculas

--- aluno.c
+++ aluno.c
@@ -1,3 +1,8 @@
+/*
+ * Guilherme Melo Gratz 			2211068
+ * João Miguel Rodrigues Montenegro da Franca	2411289
+ * Lorena Silveira dos Santos 			2312667
+*/
 // Modulo Aluno
 
 #include "aluno.h"
--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 /*
- * Guilherme Melo Gratz 	2211068
- * João Miguel 
- * */
+ * Guilherme Melo Gratz 			2211068
+ * João Miguel Rodrigues Montenegro da Franca	2411289
+ * Lorena Silveira dos Santos 			2312667
+*/
 
 // POR ENQUANTO ACHO MELHOR NAO EDITARMOS AQUI, CADA UM FAZ SEU TESTE E DPS A GNT JUNTA
 //
